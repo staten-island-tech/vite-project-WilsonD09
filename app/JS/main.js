cce2b4bcfb8987fe9ba4bb285f67f0322d6c1e34
WilsonD09 <wilsond34@nycstudents.net>
2024-11-04 12:28:29 -0500

    s

--- app/JS/main.js
+++ app/JS/main.js
@@ -1,23 +1,21 @@
 import "../CSS/style.css";
 import { gpu } from "./items";
 
-cardNum = 1;
+let cardNum = 1;
 
-function cardCreate() {
-  gpu.forEach((i) =>
+gpu.forEach(
+  (el) =>
     document.querySelector(".container").insertAdjacentHTML(
       "beforeend",
-      `<div class="card" id="card-${cardNum}"><h3>${i["title"]}</h3><ul ><h5>Information:</h5>
-        <li class="liSize">Release Year: ${i["releaseYear"]}</li>
-        <li class="liSize">Price: ${i["price"]}</li>
-        <li class="liSize">FPS in 1080p: ${i["fps1080p"]}</li>
-        <li class="liSize">Company: ${i["company"]}</li>
+      `<div class="card" id="card-${cardNum}"><h3>${el["title"]}</h3>
+        <img src=${el["imageUrl"]} alt="${el["altText"]}>
+        <ul><h5>Information:</h5>
+        <li class="liSize">Release Year: ${el["releaseYear"]}</li>
+        <li class="liSize">Price: ${el["price"]}</li>
+        <li class="liSize">FPS in 1080p: ${el["fps1080p"]}</li>
+        <li class="liSize">Company: ${el["company"]}</li>
     </ul>
-        </div>`
-    )
-  );
-}
-if (i["company"] === "AMD") {
-  document.querySelector(`#card-${cardNum}`).style.backgroundColor = "red";
-} else {
-}
+    </div>`
+    ),
+  cardNum++
+);
