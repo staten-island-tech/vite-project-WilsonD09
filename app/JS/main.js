2b69e9dc364a3692e1d9720dcfc263bb21226a5c
WilsonD09 <wilsond34@nycstudents.net>
2024-11-07 12:28:33 -0500

    s

--- app/JS/main.js
+++ app/JS/main.js
@@ -3,19 +3,30 @@ import { gpu } from "./items";
 
 let cardNum = 1;
 
-gpu.forEach(
-  (el) =>
-    document.querySelector(".container").insertAdjacentHTML(
-      "beforeend",
-      `<div class="card" id="card-${cardNum}"><h3>${el["title"]}</h3>
-        <img class="img" src=${el["imageUrl"]} alt="${el["altText"]}>
-        <ul><h5>Information:</h5>
-        <li class="liSize">Release Year: ${el["releaseYear"]}</li>
-        <li class="liSize">Price: ${el["price"]}</li>
-        <li class="liSize">FPS in 1080p: ${el["fps1080p"]}</li>
-        <li class="liSize">Company: ${el["company"]}</li>
-    </ul>
-    </div>`
-    ),
-  cardNum++
-);
+function createCards() {
+  gpu.forEach(
+    (el) => (
+      document.querySelector(".container").insertAdjacentHTML(
+        "beforeend",
+        `<div class="card" id="card-${cardNum}"><h3>${el["title"]}</h3>
+          <img class="img" src=${el["imageUrl"]} alt="${el["altText"]}">
+          
+          <h5>Information:</h5>
+          <ul>
+          <li class="liSize">Release Year: ${el["releaseYear"]}</li>
+          <li class="liSize">Price: ${el["price"]}</li>
+          <li class="liSize">FPS in 1080p: ${el["fps1080p"]}</li>
+          <li class="liSize">Company: ${el["company"]}</li></ul></div>`
+      ),
+      (cardNum += 1)
+    )
+  );
+}
+
+function leastGreatest() {
+  document.getElementById("#");
+}
+createCards();
+
+/* .sort((x, y) => x.price - y.price)
+ */
